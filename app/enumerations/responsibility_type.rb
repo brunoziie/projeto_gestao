c34c4b9b1577d850eb3a1ae26c7dabf496fbac4a
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2016-03-29 11:55:48 -0300

    Responsibility enum

---
+++ app/enumerations/responsibility_type.rb
@@ -0,0 +1,5 @@
+class ResponsibilityType < EnumerateIt::Base
+  associate_values manager: 1, worker: 2
+end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  extend EnumerateIt
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  has_enumeration_for :responsibility, with: ResponsibilityType, create_helpers: true
 end
